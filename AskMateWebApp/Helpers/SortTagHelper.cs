ba3f5c21dd4d86ef91079c8ad1e6c862c936f4db
Kohányi Róbert <kohanyi.robert@gmail.com>
2020-04-18 23:17:46 +0200

    Highlight active sort field

---
+++ AskMateWebApp/Helpers/SortTagHelper.cs
@@ -0,0 +1,33 @@
+using AskMateWebApp.Domain;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+using System;
+
+namespace AskMateWebApp.Helpers
+{
+    [HtmlTargetElement(Attributes = "sort")]
+    public class SortTagHelper : TagHelper
+    {
+        [ViewContext]
+        public ViewContext ViewContext { get; set; }
+
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            var sort = ViewContext.HttpContext.Request.Query["sort"];
+            var ascending = ViewContext.HttpContext.Request.Query["ascending"];
+
+            string modelSort = sort.Count == 0 ? "SubmissionTime" : sort[0];
+            string modelAscending = ascending.Count == 0 ? "false" : ascending[0];
+
+            string routeSort = context.AllAttributes["asp-route-sort"].Value.ToString();
+            string routeAscending = context.AllAttributes["asp-route-ascending"].Value.ToString();
+
+            output.Attributes.RemoveAll("sort");
+            if (routeSort.Equals(modelSort) && routeAscending.Equals(modelAscending))
+            {
+                output.Attributes.SetAttribute("class", "active");
+            }
+        }
+    }
+}
